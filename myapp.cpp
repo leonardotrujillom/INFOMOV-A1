9743ff941b23f98f90fdd90bfe05ea80bd790aef
Leonardo Trujillo <leonardo.trujillo.m@gmail.com>
2022-05-05 10:26:43 +0200

    Flag exaggeration + first analysis

--- myapp.cpp
+++ myapp.cpp
@@ -7,61 +7,67 @@ TheApp* CreateApp() { return new MyApp(); }
 // -----------------------------------------------------------
 void MyApp::Init()
 {
-	// load tank sprites
-	tank1 = new Sprite( "assets/tanks.png", make_int2( 128, 100 ), make_int2( 310, 360 ), 36, 256 );
-	tank2 = new Sprite( "assets/tanks.png", make_int2( 327, 99 ), make_int2( 515, 349 ), 36, 256 );
-	// load bush sprite for dust streams
-	bush[0] = new Sprite( "assets/bush1.png", make_int2( 2, 2 ), make_int2( 31, 31 ), 10, 256 );
-	bush[1] = new Sprite( "assets/bush2.png", make_int2( 2, 2 ), make_int2( 31, 31 ), 14, 256 );
-	bush[2] = new Sprite( "assets/bush3.png", make_int2( 2, 2 ), make_int2( 31, 31 ), 20, 256 );
-	bush[0]->ScaleAlpha( 96 );
-	bush[1]->ScaleAlpha( 64 );
-	bush[2]->ScaleAlpha( 128 );
-	// pointer
-	pointer = new SpriteInstance( new Sprite( "assets/pointer.png" ) );
-	// create armies
-	for (int y = 0; y < 16; y++) for (int x = 0; x < 16; x++)
-	{
-		Actor* army1Tank = new Tank( tank1, make_int2( 520 + x * 32, 2420 - y * 32 ), make_int2( 5000, -500 ), 0, 0 );
-		Actor* army2Tank = new Tank( tank2, make_int2( 3300 - x * 32, y * 32 + 700 ), make_int2( -1000, 4000 ), 10, 1 );
-		actorPool.push_back( army1Tank );
-		actorPool.push_back( army2Tank );
-	}
-	for (int y = 0; y < 12; y++) for (int x = 0; x < 12; x++)
-	{
-		Actor* army1Tank = new Tank( tank1, make_int2( 40 + x * 32, 2620 - y * 32 ), make_int2( 5000, -500 ), 0, 0 );
-		Actor* army2Tank = new Tank( tank2, make_int2( 3900 - x * 32, y * 32 + 300 ), make_int2( -1000, 4000 ), 10, 1 );
-		actorPool.push_back( army1Tank );
-		actorPool.push_back( army2Tank );
-	}
-	for (int y = 0; y < 8; y++) for (int x = 0; x < 8; x++)
-	{
-		Actor* army1Tank = new Tank( tank1, make_int2( 1440 + x * 32, 2220 - y * 32 ), make_int2( 3500, -500 ), 0, 0 );
-		Actor* army2Tank = new Tank( tank2, make_int2( 2400 - x * 32, y * 32 + 900 ), make_int2( 1300, 4000 ), 128, 1 );
-		actorPool.push_back( army1Tank );
-		actorPool.push_back( army2Tank );
-	}
-	// load mountain peaks
-	Surface mountains( "assets/peaks.png" );
-	for (int y = 0; y < mountains.height; y++) for (int x = 0; x < mountains.width; x++)
-	{
-		uint p = mountains.pixels[x + y * mountains.width];
-		if ((p & 0xffff) == 0) peaks.push_back( make_float3( make_int3( x * 8, y * 8, (p >> 16) & 255 ) ) );
-	}
-	// add sandstorm
-	for( int i = 0; i < 7500; i++ )
-	{
-		int x = RandomUInt() % map.bitmap->width;
-		int y = RandomUInt() % map.bitmap->height;
-		int d = (RandomUInt() & 15) - 8;
-		sand.push_back( new Particle( bush[i % 3], make_int2( x, y ), map.bitmap->pixels[x + y * map.bitmap->width], d ) );
-	}
+	//// load tank sprites
+	//tank1 = new Sprite( "assets/tanks.png", make_int2( 128, 100 ), make_int2( 310, 360 ), 36, 256 );
+	//tank2 = new Sprite( "assets/tanks.png", make_int2( 327, 99 ), make_int2( 515, 349 ), 36, 256 );
+	//// load bush sprite for dust streams
+	//bush[0] = new Sprite( "assets/bush1.png", make_int2( 2, 2 ), make_int2( 31, 31 ), 10, 256 );
+	//bush[1] = new Sprite( "assets/bush2.png", make_int2( 2, 2 ), make_int2( 31, 31 ), 14, 256 );
+	//bush[2] = new Sprite( "assets/bush3.png", make_int2( 2, 2 ), make_int2( 31, 31 ), 20, 256 );
+	//bush[0]->ScaleAlpha( 96 );
+	//bush[1]->ScaleAlpha( 64 );
+	//bush[2]->ScaleAlpha( 128 );
+	//// pointer
+	//pointer = new SpriteInstance( new Sprite( "assets/pointer.png" ) );
+	//// create armies
+	//for (int y = 0; y < 16; y++) for (int x = 0; x < 16; x++)
+	//{
+	//	Actor* army1Tank = new Tank( tank1, make_int2( 520 + x * 32, 2420 - y * 32 ), make_int2( 5000, -500 ), 0, 0 );
+	//	Actor* army2Tank = new Tank( tank2, make_int2( 3300 - x * 32, y * 32 + 700 ), make_int2( -1000, 4000 ), 10, 1 );
+	//	actorPool.push_back( army1Tank );
+	//	actorPool.push_back( army2Tank );
+	//}
+	//for (int y = 0; y < 12; y++) for (int x = 0; x < 12; x++)
+	//{
+	//	Actor* army1Tank = new Tank( tank1, make_int2( 40 + x * 32, 2620 - y * 32 ), make_int2( 5000, -500 ), 0, 0 );
+	//	Actor* army2Tank = new Tank( tank2, make_int2( 3900 - x * 32, y * 32 + 300 ), make_int2( -1000, 4000 ), 10, 1 );
+	//	actorPool.push_back( army1Tank );
+	//	actorPool.push_back( army2Tank );
+	//}
+	//for (int y = 0; y < 8; y++) for (int x = 0; x < 8; x++)
+	//{
+	//	Actor* army1Tank = new Tank( tank1, make_int2( 1440 + x * 32, 2220 - y * 32 ), make_int2( 3500, -500 ), 0, 0 );
+	//	Actor* army2Tank = new Tank( tank2, make_int2( 2400 - x * 32, y * 32 + 900 ), make_int2( 1300, 4000 ), 128, 1 );
+	//	actorPool.push_back( army1Tank );
+	//	actorPool.push_back( army2Tank );
+	//}
+	//// load mountain peaks
+	//Surface mountains( "assets/peaks.png" );
+	//for (int y = 0; y < mountains.height; y++) for (int x = 0; x < mountains.width; x++)
+	//{
+	//	uint p = mountains.pixels[x + y * mountains.width];
+	//	if ((p & 0xffff) == 0) peaks.push_back( make_float3( make_int3( x * 8, y * 8, (p >> 16) & 255 ) ) );
+	//}
+	//// add sandstorm
+	//for( int i = 0; i < 7500; i++ )
+	//{
+	//	int x = RandomUInt() % map.bitmap->width;
+	//	int y = RandomUInt() % map.bitmap->height;
+	//	int d = (RandomUInt() & 15) - 8;
+	//	sand.push_back( new Particle( bush[i % 3], make_int2( x, y ), map.bitmap->pixels[x + y * map.bitmap->width], d ) );
+	//}
 	// place flags
 	Surface* flagPattern = new Surface( "assets/flag.png" );
-	VerletFlag* flag1 = new VerletFlag( make_int2( 3000, 848 ), flagPattern );
-	actorPool.push_back( flag1 );
-	VerletFlag* flag2 = new VerletFlag( make_int2( 1076, 1870 ), flagPattern );
-	actorPool.push_back( flag2 );
+
+	for (int i = 0; i < 1000; i++) {
+		int randX = rand() % 3000, randY = rand() % 2000;
+		VerletFlag* flag1 = new VerletFlag(make_int2(randX, randY), flagPattern);
+		actorPool.push_back(flag1);
+	}
+	// VerletFlag* flag1 = new VerletFlag( make_int2( 3000, 848 ), flagPattern );
+	// actorPool.push_back( flag1 );
+	// VerletFlag* flag2 = new VerletFlag( make_int2( 1076, 1870 ), flagPattern );
+	// actorPool.push_back( flag2 );
 	// initialize map view
 	map.UpdateView( screen, zoom );
 }
@@ -116,10 +122,10 @@ void MyApp::Tick( float deltaTime )
 	grid.Clear();
 	grid.Populate( actorPool );
 	// update and render actors
-	pointer->Remove();
-	for (int s = (int)sand.size(), i = s - 1; i >= 0; i--) sand[i]->Remove();
+	//pointer->Remove();
+	//for (int s = (int)sand.size(), i = s - 1; i >= 0; i--) sand[i]->Remove();
 	for (int s = (int)actorPool.size(), i = s - 1; i >= 0; i--) actorPool[i]->Remove();
-	for (int s = (int)sand.size(), i = 0; i < s; i++) sand[i]->Tick();
+	//for (int s = (int)sand.size(), i = 0; i < s; i++) sand[i]->Tick();
 	for (int i = 0; i < (int)actorPool.size(); i++) if (!actorPool[i]->Tick())
 	{
 		// actor got deleted, replace by last in list
@@ -132,9 +138,9 @@ void MyApp::Tick( float deltaTime )
 	}
 	coolDown++;
 	for (int s = (int)actorPool.size(), i = 0; i < s; i++) actorPool[i]->Draw();
-	for (int s = (int)sand.size(), i = 0; i < s; i++) sand[i]->Draw();
+	//for (int s = (int)sand.size(), i = 0; i < s; i++) sand[i]->Draw();
 	int2 cursorPos = map.ScreenToMap( mousePos );
-	pointer->Draw( map.bitmap, make_float2( cursorPos ), 0 );
+	//pointer->Draw( map.bitmap, make_float2( cursorPos ), 0 );
 	// handle mouse
 	HandleInput();
 	// report frame time
